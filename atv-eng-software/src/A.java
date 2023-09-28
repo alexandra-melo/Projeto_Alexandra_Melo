6926f3049e76da8a29601d4dc9988b0b2c68a1de
Alexandra Melo <100172257+alexandra-melo@users.noreply.github.com>
2023-09-28 20:21:56 -0300

    -m

---
+++ atv-eng-software/src/A.java
@@ -0,0 +1,26 @@
+public class A {
+    private float a1;
+    private float a2;
+
+    public float getA1() {
+        return a1;
+    }
+    public void setA1(float a1) {
+        this.a1 = a1;
+    }
+    public float getA2() {
+        return a2;
+    }
+    public void setA2(float a2) {
+        this.a2 = a2;
+    }
+
+    public void MA1(){
+        System.out.println("MA1");
+    }
+
+    public void MA2(){
+        System.out.println("MA2");
+    }
+    
+}
---
+++ atv-eng-software/src/B.java
@@ -0,0 +1,29 @@
+public class B {
+    private float b1;
+    private float b2;
+
+    public float getB1() {
+        return b1;
+    }
+
+    public void setB1(float b1) {
+        this.b1 = b1;
+    }
+
+    public float getB2() {
+        return b2;
+    }
+
+    public void setB2(float b2) {
+        this.b2 = b2;
+    }
+
+     public void MB1(){
+        System.out.println("MB1");
+    }
+
+    public void MB2(){
+        System.out.println("MB2");
+    }   
+    
+}
