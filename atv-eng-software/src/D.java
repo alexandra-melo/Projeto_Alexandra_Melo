619d4f03de82889c6aa5829a167fda4a3c10fca0
Alexandra Melo <100172257+alexandra-melo@users.noreply.github.com>
2023-10-03 19:32:46 -0300

    atualizacao
    Atualizacao das classes B, C e D

---
+++ atv-eng-software/src/D.java
@@ -0,0 +1,13 @@
+public class D {
+    private float c1;
+    private float c2;
+
+    public void MD1() {
+        System.out.println("MC1");
+    }
+
+    public void MD2() {
+        System.out.println("MC2");
+    }
+
+}
