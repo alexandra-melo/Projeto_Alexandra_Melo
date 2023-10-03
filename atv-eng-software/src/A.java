63cf693af18424ec73c411673aecc52886af0fa9
Alexandra Melo <100172257+alexandra-melo@users.noreply.github.com>
2023-10-03 18:57:25 -0300

    ATT
    Criacao do metodo MA3

--- atv-eng-software/src/A.java
+++ atv-eng-software/src/A.java
@@ -5,22 +5,29 @@ public class A {
     public float getA1() {
         return a1;
     }
+
     public void setA1(float a1) {
         this.a1 = a1;
     }
+
     public float getA2() {
         return a2;
     }
+
     public void setA2(float a2) {
         this.a2 = a2;
     }
 
-    public void MA1(){
+    public void MA1() {
         System.out.println("MA1");
     }
 
-    public void MA2(){
+    public void MA2() {
         System.out.println("MA2");
     }
-    
+
+    public void MA3() {
+        System.out.println("MA3");
+    }
+
 }
