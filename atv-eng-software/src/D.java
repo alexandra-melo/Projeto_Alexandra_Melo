a32361978395c33f3a2c7f3cd0374649dfecb0b4
Alexandra Melo <100172257+alexandra-melo@users.noreply.github.com>
2023-10-03 19:49:48 -0300

    Update D.java

--- atv-eng-software/src/D.java
+++ atv-eng-software/src/D.java
@@ -3,11 +3,14 @@ public class D {
     private float c2;
 
     public void MD1() {
-        System.out.println("MC1");
+        System.out.println("MD1");
     }
 
     public void MD2() {
-        System.out.println("MC2");
+        System.out.println("MD2");
     }
 
+    public void MD3() {
+        System.out.println("MD3");
+    }
 }
