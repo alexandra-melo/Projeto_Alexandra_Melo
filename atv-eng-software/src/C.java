3cffb56d01952773d63f29ba6aadd3a40621efba
Alexandra Melo <100172257+alexandra-melo@users.noreply.github.com>
2023-10-03 18:55:17 -0300

    att
    Criação da Classe C

---
+++ atv-eng-software/src/C.java
@@ -0,0 +1,13 @@
+public class C {
+    private float c1;
+    private float c2;
+
+    public void MC1() {
+        System.out.println("MC1");
+    }
+
+    public void MC2() {
+        System.out.println("MC2");
+    }
+
+}
